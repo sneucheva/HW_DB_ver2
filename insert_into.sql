258101f67d9ff1f877b9cb0816eb1c0f58d7127b
sneucheva <121288212+sneucheva@users.noreply.github.com>
2023-03-24 01:42:55 +0300

    2023-03-23

--- insert_into.sql
+++ insert_into.sql
@@ -35,24 +35,25 @@ values ('Top 100 Blues', '2018'), ('BBC Top Of The Pops 1976', '1976'),('BBC Top
 select * from collections;       
     
 insert into tracks (track_name, track_duration, id_album)
-values ('It''s Been A Long, Long Time', '2:00', NULL), ('Be Friend', '3:45', 1),
-       ('Lovely Day', '6:14', 1), ('Blues Boys Tune', '3:29', Null),
-       ('I''m King', '2:20', 2), ('Good Man Gon Bad', '2:48', 2),
-       ('Needles And Pins', '2:00', 3), ('It''s My Life', '4:12', 5),
-       ('She Don''t Know Me', '4:02', 4), ('You Give Love A Bad Name', '3:53', 5),
-       ('See Red', '3:02', Null), ('Shiesty Season Intro', '1:47', 6),
-       ('Uptown Top Ranking', '3:28', null), ('Fallow Alhea & Donna', '3:35', 7),
-       ('Cali Groun', '3:57', 8), ('main Slime', '3:12', 8),
-       ('Can''t Make Up My Mind', '3:03', Null), ('Something On so Right', '3:06', Null);
+values ('It''s Been A Long, Long Time', '00:2:00', NULL), ('Be Friend', '00:03:45', 1),
+       ('Lovely Day', '00:06:14', 1), ('Blues Boys Tune', '00:03:29', Null),
+       ('I''m King', '00:02:20', 2), ('Good Man Gon Bad', '00:02:48', 2),
+       ('Needles And Pins', '00:02:00', 3), ('It''s My Life', '00:04:12', 5),
+       ('She Don''t Know Me', '00:04:02', 4), ('You Give Love A Bad Name', '00:03:53', 5),
+       ('See Red', '00:03:02', Null), ('Shiesty Season Intro', '00:01:47', 6),
+       ('Uptown Top Ranking', '00:03:28', null), ('Fallow Alhea & Donna', '00:03:35', 7),
+       ('Cali Groun', '00:03:57', 8), ('main Slime', '00:03:12', 8),
+       ('Can''t Make Up My Mind', '00:03:03', Null), ('Something On so Right', '00:03:06', Null);
 
---select * from tracks
---drop table track_collections     
---drop table tracks
+select * from tracks;
+    
+drop table tracks;
 
 insert into track_collections (id_track, id_collection)
 values (1, 7), (4, 7), (11, 10), (13, 11), (17, 12), (18, 12);
 
 select * from track_collections; 
---drop table  track_collections 
+
+drop table  track_collections; 
        
        
